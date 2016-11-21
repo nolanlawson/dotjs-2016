4a7a499dd1b84301fc43ba334c5221fb971538b5
Nolan Lawson <nolan.lawson@gmail.com>
2016-11-20 19:26:07 -0800

    fix sw.js

--- sw.js
+++ sw.js
@@ -2,10 +2,10 @@ importScripts('https://unpkg.com/sw-toolbox@3.4.0/sw-toolbox.js');
 
 toolbox.options.debug = true;
 toolbox.precache([
-  '/',
-  '/readme.md',
-  '/script.js',
-  '/style.css',
+  './',
+  './readme.md',
+  './script.js',
+  './style.css',
   'https://remarkjs.com/downloads/remark-latest.min.js'
 ])
 toolbox.router.get('/(.*)', toolbox.fastest, {
