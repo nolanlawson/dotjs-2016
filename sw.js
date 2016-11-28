86a1d0253530277c45ab9bc2046411a955cd13c7
Nolan Lawson <nolan.lawson@gmail.com>
2016-11-28 08:53:02 -0800

    precache images

--- sw.js
+++ sw.js
@@ -1,13 +1,20 @@
-importScripts('https://unpkg.com/sw-toolbox@3.4.0/sw-toolbox.js');
+importScripts('https://unpkg.com/sw-toolbox@3.4.0/sw-toolbox.js')
+toolbox.options.debug = true
 
-toolbox.options.debug = true;
 toolbox.precache([
   './',
   './readme.md',
   './script.js',
   './style.css',
-  'https://remarkjs.com/downloads/remark-latest.min.js'
-]);
+  'https://remarkjs.com/downloads/remark-latest.min.js',
+  'img/app_store.png',
+  'img/bowie.jpg',
+  'img/flash.png',
+  'img/google_play.png',
+  'img/slight.png',
+  'img/webworks.png'
+])
+
 toolbox.router.get('/(.*)', toolbox.fastest, {
   origin: /.*/
-});
+})
